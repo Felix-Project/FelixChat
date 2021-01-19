c5f85d62e55b2f18f6a75902152748431e12ecb6
01384319（黄明发） <mingfahuang@sfmail.sf-express.com>
2021-01-19 11:56:11 +0800

    Initial commit

---
+++ app/src/main/java/com/felix/felixchat/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.felix.felixchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val json:JSONObject
+    }
+}
